4bc83908d9c98e4df64e8f0685cf714f0eaec7f7
Claire <87537970+Chiarilde@users.noreply.github.com>
2022-10-05 19:20:18 +0200

    Add files via upload

---
+++ Group Projects/w2d2ex1.py
@@ -0,0 +1,3 @@
+stringa = str(input('Inserisci una stringa:')).upper()
+new_stringa = stringa.replace('A','a').replace('E','e').replace('I','i').replace('O','o').replace('U','U')
+print(new_stringa)
