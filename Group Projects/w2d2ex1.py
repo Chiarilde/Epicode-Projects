f88098464d416ba157b2d2daa05b6847dd11150c
Claire <87537970+Chiarilde@users.noreply.github.com>
2022-10-05 19:25:28 +0200

    Delete w2d2ex1.py

--- Group Projects/w2d2ex1.py
+++
@@ -1,3 +0,0 @@
-stringa = str(input('Inserisci una stringa:')).upper()
-new_stringa = stringa.replace('A','a').replace('E','e').replace('I','i').replace('O','o').replace('U','U')
-print(new_stringa)
